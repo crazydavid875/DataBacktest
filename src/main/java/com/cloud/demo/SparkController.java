5cc0a6c5a3dd02ac4e0d1e6f8e852823cd8f918f
han1018 <paul52841018@hotmail.com>
2021-06-23 02:34:46 +0800

    init

--- src/main/java/com/cloud/demo/SparkController.java
+++ src/main/java/com/cloud/demo/SparkController.java
@@ -10,25 +10,22 @@ import org.apache.spark.sql.SparkSession;
 public class SparkController {
     private static String appName = "spark.sql.demo";
     private static String master = "local[*]";
-    //"hdfs://localhost:9000/hadoop1/stock2330.csv"
+    // "hdfs://localhost:9000/hadoop1/stock2330.csv"
     private static SparkSession spark;
+
     public SparkController() {
-    	 SparkSession spark = SparkSession
-                 .builder()
-                 .appName(appName)
-                 .master(master)
-                 .getOrCreate();
+        SparkSession spark = SparkSession.builder().appName(appName).master(master).getOrCreate();
     }
-    public String run(String path){
-       
-                
+
+    public String run(String path) {
+
         String str = "";
-        //读取元数据文件
-        Dataset<Row> df = spark.read().csv("hdfs://localhost:9000/"+path);
-        //生成rdd
-        
+        // 读取元数据文件
+        Dataset<Row> df = spark.read().csv("hdfs://localhost:9000/" + path);
+        // 生成rdd
+
         str = df.toString();
-        
+
         return str;
     }
 }
