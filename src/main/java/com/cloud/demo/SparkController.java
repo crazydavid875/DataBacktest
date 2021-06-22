b91044465c2a0620e772fef5a4ae901ac8547e16
david <crazydavid875@gmail.com>
2021-06-22 06:47:42 -0700

    fix

--- src/main/java/com/cloud/demo/SparkController.java
+++ src/main/java/com/cloud/demo/SparkController.java
@@ -11,26 +11,24 @@ public class SparkController {
     private static String appName = "spark.sql.demo";
     private static String master = "local[*]";
     //"hdfs://localhost:9000/hadoop1/stock2330.csv"
+    private static SparkSession spark;
+    public SparkController() {
+    	 SparkSession spark = SparkSession
+                 .builder()
+                 .appName(appName)
+                 .master(master)
+                 .getOrCreate();
+    }
     public String run(String path){
-        SparkSession spark = SparkSession
-                .builder()
-                .appName(appName)
-                .master(master)
-                .getOrCreate();
+       
                 
         String str = "";
         //读取元数据文件
-        Dataset<Row> df = spark.read().csv("hdfs://localhost:9000/hadoop1/"+path);
+        Dataset<Row> df = spark.read().csv("hdfs://localhost:9000/"+path);
         //生成rdd
-        JavaRDD<Row> rdd = df.toJavaRDD();
-        //遍历
-        rdd.foreach(new VoidFunction<Row>() {
-            public void call(Row row) throws Exception {
-                System.out.println(row.toString());
-            }
-        });
+        
         str = df.toString();
-        spark.stop();
+        
         return str;
     }
 }
